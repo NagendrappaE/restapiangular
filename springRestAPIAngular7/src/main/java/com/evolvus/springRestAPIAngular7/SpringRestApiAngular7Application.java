ba822b498867e16cb2bd4f6812bcdbb68d3fbc76
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2020-08-05 12:31:43 +0530

    spring rest api

---
+++ springRestAPIAngular7/src/main/java/com/evolvus/springRestAPIAngular7/SpringRestApiAngular7Application.java
@@ -0,0 +1,13 @@
+package com.evolvus.springRestAPIAngular7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiAngular7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiAngular7Application.class, args);
+	}
+
+}
---
+++ springRestAPIAngular7/src/test/java/com/evolvus/springRestAPIAngular7/SpringRestApiAngular7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolvus.springRestAPIAngular7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiAngular7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
